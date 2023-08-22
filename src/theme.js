1539218c264169ef91df6b4214f8648dff446394
jervisGit <jlee045@mymail.sim.edu.sg>
2023-08-22 21:18:31 +0800

    remove redundant theme warning

--- src/theme.js
+++ src/theme.js
@@ -363,11 +363,7 @@ export const success = withAlphas({
 });
   
 export const warning = withAlphas({
-    lightest: '#FFFAEB',
-    light: '#FEF0C7',
     main: '#F79009',
-    dark: '#B54708',
-    darkest: '#7A2E0E',
     contrastText: '#FFFFFF'
 });
   

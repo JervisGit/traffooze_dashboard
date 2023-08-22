e3d1224c2d51b9ac80b74c91fe8e1eb40126b49f
sashank2711 <sashank2711@gmail.com>
2023-08-22 12:14:50 +0800

    modified:   src/FavoriteLocations.jsx 	new file:   src/favoriteLocationsData.js

---
+++ src/favoriteLocationsData.js
@@ -0,0 +1,21 @@
+const favLocations = [
+    {
+      title: 'Home',
+      Address: 'Clementi Road',
+    },
+    {
+      title: 'Work',
+      Address: '123 Business Street',
+    },
+    {
+      title: 'Home',
+      Address: 'Park Avenue, Apt 5',
+    },
+    {
+      title: 'Work',
+      Address: '789 Office Lane',
+    },
+  ];
+  
+  export default favLocations;
+  
